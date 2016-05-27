c24189cd286f16c96c5d9b9e5853450c886f6928
Martijn Daniels <martijn.daniels@student.fontys.nl>
2016-05-27 14:19:09 +0200

    appel spawner jwz

---
+++ SUMmerHarvest/Assets/Scripts/Game/GameObjects/Apple.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Apple : MonoBehaviour
+{
+
+
+    public int keepHanging;
+    public int minValue;
+    public int maxValue;
+    public float speed;
+    public int scoreValue;
+    private int h = 0;
+    private bool drp;
+    private scoreApple sA;
+
+    void Start()
+    {
+        NewScore();
+    }
+    void Update()
+    {
+        if (!drp)
+        {
+            if (h <= keepHanging)
+            {
+                h++;
+            }
+            else
+            {
+                Drop();
+            }
+        }
+        else
+        {
+            Vector3 p = this.transform.position;
+            p.Set(p.x, p.y - speed, p.z);
+            this.transform.position = p;
+        }
+    }
+
+    public void Drop()
+    {
+        drp = true;
+    }
+
+    public void Pickup(Basket b)
+    {
+        b.RemoveScore(scoreValue);
+        Destroy();
+    }
+
+    public void Destroy()
+    {
+        GameObject.Destroy(sA.gameObject);
+        GameObject.Destroy(this.gameObject);
+    }
+    public int GetNumber()
+    {
+        return scoreValue;
+    }
+
+    public void SetAppleUI(scoreApple s)
+    {
+        sA = s;
+    }
+    void NewScore()
+    {
+        System.Random r = new System.Random();
+        scoreValue = r.Next(minValue, maxValue);
+        if (scoreValue == 0)
+        {
+            NewScore();
+        }
+    }
+    public bool IsFalling()
+    {
+        return drp;
+    }
+}
---
+++ SUMmerHarvest/Assets/Scripts/Game/UI/InWorld/scoreApple.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scoreApple : MonoBehaviour
+{
+    public float YAXisOffset;
+    private Apple a;
+    private Text t;
+    private RectTransform rect;
+    void Start()
+    {
+        t = gameObject.GetComponent<Text>();
+        rect = t.GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (a != null)
+        {
+            t.text = a.GetNumber().ToString();
+            rect.position = new Vector3(a.transform.position.x, a.transform.position.y + YAXisOffset, transform.position.z);
+        }
+    }
+    public void SetApple(Apple ap)
+    {
+        a = ap;
+        a.SetAppleUI(this);
+    }
+}

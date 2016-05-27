0e2664943d036e0c9f8c11ea6bd5173d7c63a849
Anton van Dijk <anton.van.dijk@live.nl>
2016-05-27 09:16:14 +0200

    Added basic menu and game scene

---
+++ SUMmerHarvest/Assets/Scripts/Game/UI/Menus/MenuMain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Game.UI.Menus
+{
+    public class MenuMain : MonoBehaviour
+    {
+        /// <summary>
+        /// Loads the 'Game' scene.
+        /// </summary>
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        /// <summary>
+        /// Closes the game (or exists play-mode).
+        /// </summary>
+        public void CloseGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+        }
+    }
+}

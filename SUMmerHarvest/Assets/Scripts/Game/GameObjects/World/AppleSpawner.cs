4ed74b9277b0f2168c03887eb2e0a924649bf4a9
TheRealGLH <TheRealGLH@users.noreply.github.com>
2016-06-03 10:19:56 +0200

    Appelspawner 2: Electric Boogaloo (#3)
    * applespawner fix+rigidbody apple
    
    * fix appelspawner

--- SUMmerHarvest/Assets/Scripts/Game/GameObjects/World/AppleSpawner.cs
+++ SUMmerHarvest/Assets/Scripts/Game/GameObjects/World/AppleSpawner.cs
@@ -11,13 +11,13 @@ public class AppleSpawner : MonoBehaviour {
     public Canvas InworldCanvas;
     public int maxAppleLoops;
     int appleLoops = 0;
-    private int currAppleLoops;
     public GameObject gameWorld;//za warudo
     private List<Apple> apples = new List<Apple>();
+    System.Random random;
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (appleTicker <= spawnRate)
         {
@@ -36,13 +36,17 @@ public class AppleSpawner : MonoBehaviour {
             GameObject.Destroy(a.gameObject);
         }
         apples = new List<Apple>();
+        random = new System.Random();
+
     }
 
     public void SpawnApple()
     {
         GameObject a = Instantiate(applePrefab);
         a.transform.SetParent(gameWorld.transform, false);
-        
+
+        StartCoroutine(a.GetComponent<Apple>().Drop());
+
         GameObject aU = Instantiate(appleUIPrefab);
         aU.transform.SetParent(InworldCanvas.transform);
         aU.GetComponent<ScoreApple>().SetApple(a.GetComponent<Apple>());
@@ -52,15 +56,17 @@ public class AppleSpawner : MonoBehaviour {
 
     void setApplepos(GameObject appleOBJ)
     {
-        appleOBJ.transform.position = RandomHelper.RandomVector3(spawnArea.bounds.min, spawnArea.bounds.max);
-        System.Random random = new System.Random();//geen unity random :^)
+        appleOBJ.transform.position = RandomHelper.RandomVector3(spawnArea.bounds.min, spawnArea.bounds.max);     
         Apple aa = appleOBJ.GetComponent<Apple>();
         int n = random.Next(aa.minRadius,aa.maxRadius);
         Collider[] hitColliders = Physics.OverlapSphere(appleOBJ.transform.position,(float)n );
         
             int i = 0;
         while (i < hitColliders.Length) {
-            if (hitColliders[i].tag=="Apple"&&!appleOBJ==hitColliders[i])//we don't want to collide with other apples, but we obviously have to ignore ourselves. it's not as if I wasted 2 hours trying to figure out why this eouldn't work. ha ha ha
+            int ap = appleOBJ.GetInstanceID();
+            if (hitColliders[i].tag == "Apple") { int hc = hitColliders[i].GetInstanceID(); }
+           // if (hitColliders[i].tag == "Apple") { print("my instance " + appleOBJ.GetInstanceID() + " col ID " + hitColliders[i].GetInstanceID()); }
+            if (hitColliders[i].tag=="Apple"&&appleOBJ.GetInstanceID()!=hitColliders[i].gameObject.GetInstanceID())//we don't want to collide with other apples, but we obviously have to ignore ourselves. it's not as if I wasted 2 hours trying to figure out why this eouldn't work. ha ha ha
             {
                 Debug.Log(appleOBJ.name + " collided with" + hitColliders[i].name);
                 appleLoops++;

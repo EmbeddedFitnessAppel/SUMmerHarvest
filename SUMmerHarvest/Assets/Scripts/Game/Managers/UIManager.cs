16d30a3f44877d83b8c614c51f78326cca02da34
Kas <310654@student.fontys.nl>
2016-06-09 14:35:12 +0200

    Added UI manager with functionality to set countdown text and to initialize the endscreen. Added Score property that calculates the total score of all baskets in 1 team.

---
+++ SUMmerHarvest/Assets/Scripts/Game/Managers/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager> {
+    #region EndGameOverlay
+    [SerializeField]
+    private GameObject endGameOverlay;
+
+    [SerializeField]
+    private Text whichTeamWins;
+    [SerializeField]
+    private GameObject scoreList;
+    [SerializeField]
+    private GameObject scoreListItemPrefab;
+    #endregion
+
+    #region CountdownTimer
+    [SerializeField]
+    private Text timerText;
+    #endregion
+
+    public void SetCountdownText(string value) {
+        this.timerText.text = value;
+    }
+
+
+    public void ShowEndgamePanel(List<Team> teams) {
+        teams.Sort(new FuncComparer<Team>((t1, t2) => t1.Score.CompareTo(t2.Score)));
+
+        this.whichTeamWins.text = string.Format("Team '{0}' wint!", teams[0].Name);
+
+        foreach (Team team in teams) {
+            GameObject scoreListItem = Instantiate(scoreListItemPrefab);
+            scoreListItem.transform.SetParent(scoreList.transform);
+
+            Text teamName = scoreListItem.transform.Find("TeamName").GetComponent<Text>();
+            Text teamScore = scoreListItem.transform.Find("TeamScore").GetComponent<Text>();
+            teamName.text = team.Name;
+            teamScore.text = string.Format("{0} Mandjes", team.Score);
+        }
+
+        this.endGameOverlay.SetActive(true);
+    }
+}
---
+++ SUMmerHarvest/Assets/Scripts/Utility/FuncComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FuncComparer<T> : IComparer<T> {
+    private readonly Func<T, T, int> func;
+    public FuncComparer(Func<T, T, int> comparerFunc) {
+        this.func = comparerFunc;
+    }
+
+    public int Compare(T x, T y) {
+        return this.func(x, y);
+    }
+}

3f89885f3c7a6ec735fb5548bc111e695c5abe66
TheRealGLH <TheRealGLH@users.noreply.github.com>
2016-06-10 11:19:43 +0200

    Basket text (#4)
    * player score
    
    * n
    
    * prefab fix

--- SUMmerHarvest/Assets/Scripts/Game/GameObjects/Basket.cs
+++ SUMmerHarvest/Assets/Scripts/Game/GameObjects/Basket.cs
@@ -4,6 +4,8 @@ public class Basket : Player
 {
     public GameObject playerBasket;
 
+    public GameObject scorePrefab;
+
     public int MinBasketValue;
     public int MaxBasketValue;
 
@@ -17,6 +19,13 @@ public class Basket : Player
     public bool isExtended;
     public string extendDirection;
 
+
+    void Start()
+    {
+        GameObject aU = Instantiate(scorePrefab);
+        aU.transform.SetParent(UIManager.Instance.InWorldCanvas.transform);//als dit moeilijk doet, had je maar een afspraak moeten maken over de vorm van de gamescene. anders kan ik niet weten hoe ik bij het InWorld Canvas kom
+        aU.GetComponent<ScoreBasket>().SetOwner(this.gameObject);
+    }
     public void Update()
     {
         switch (PlayerNumber)
@@ -78,5 +87,9 @@ public class Basket : Player
     public void DodgeBackward()
     {
         transform.Translate(new Vector3(0, 0, 1.8f));
+    }
+    public int GetNumber()
+    {
+        return this.BasketValue;
     }
 }
--- SUMmerHarvest/Assets/Scripts/Game/Managers/UIManager.cs
+++ SUMmerHarvest/Assets/Scripts/Game/Managers/UIManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager> {
+
+    public Canvas InWorldCanvas;
+
     #region EndGameOverlay
     [SerializeField]
     private GameObject endGameOverlay;

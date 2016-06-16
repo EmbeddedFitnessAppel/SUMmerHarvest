1fbf1ad2d52169a73c5678a8c4207cba91b81644
Anton van Dijk <anton.van.dijk@live.nl>
2016-06-16 09:18:33 +0200

    Updated game scene

--- SUMmerHarvest/Assets/Scripts/Game/Managers/UIManager.cs
+++ SUMmerHarvest/Assets/Scripts/Game/Managers/UIManager.cs
@@ -1,50 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : Singleton<UIManager> {
-
+public class UIManager : Singleton<UIManager>
+{
     public Canvas InWorldCanvas;
 
+    #region CountdownTimer
+
+    [SerializeField]
+    private Text timerText;
+
+    #endregion
+
+    public void SetCountdownText(float seconds)
+    {
+        var minutes = Mathf.FloorToInt(seconds / 60f);
+        timerText.text = string.Format("{0}:{1:00}", minutes, Mathf.FloorToInt(seconds - minutes * 60));
+    }
+
+
+    public void ShowEndgamePanel(List<Team> teams)
+    {
+        teams.Sort(new FuncComparer<Team>((t1, t2) => t2.Score.CompareTo(t1.Score)));
+
+        whichTeamWins.text = string.Format("Team '{0}' wint!", teams[0].Name);
+
+        foreach (var team in teams)
+        {
+            var scoreListItem = Instantiate(scoreListItemPrefab);
+            scoreListItem.transform.SetParent(scoreList.transform);
+
+            var teamName = scoreListItem.transform.Find("TeamName").GetComponent<Text>();
+            var teamScore = scoreListItem.transform.Find("TeamScore").GetComponent<Text>();
+            teamName.text = team.Name;
+            teamScore.text = string.Format("{0} Mandjes", team.Score);
+        }
+
+        endGameOverlay.SetActive(true);
+    }
+
     #region EndGameOverlay
+
     [SerializeField]
     private GameObject endGameOverlay;
 
     [SerializeField]
     private Text whichTeamWins;
+
     [SerializeField]
     private GameObject scoreList;
+
     [SerializeField]
     private GameObject scoreListItemPrefab;
+
     #endregion
-
-    #region CountdownTimer
-    [SerializeField]
-    private Text timerText;
-    #endregion
-
-    public void SetCountdownText(float seconds) {
-        int minutes = Mathf.FloorToInt(seconds / 60f);
-        this.timerText.text = string.Format("{0}:{1:00}", minutes, Mathf.FloorToInt(seconds - minutes * 60));
-    }
-
-
-    public void ShowEndgamePanel(List<Team> teams) {
-        teams.Sort(new FuncComparer<Team>((t1, t2) => t2.Score.CompareTo(t1.Score)));
-
-        this.whichTeamWins.text = string.Format("Team '{0}' wint!", teams[0].Name);
-
-        foreach (Team team in teams) {
-            GameObject scoreListItem = Instantiate(scoreListItemPrefab);
-            scoreListItem.transform.SetParent(scoreList.transform);
-
-            Text teamName = scoreListItem.transform.Find("TeamName").GetComponent<Text>();
-            Text teamScore = scoreListItem.transform.Find("TeamScore").GetComponent<Text>();
-            teamName.text = team.Name;
-            teamScore.text = string.Format("{0} Mandjes", team.Score);
-        }
-
-        this.endGameOverlay.SetActive(true);
-    }
 }

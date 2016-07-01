0df32609c2331bacb2295c674a0d8ad8d965e4ad
Anton van Dijk <anton.van.dijk@live.nl>
2016-07-01 15:00:10 +0200

    Added camera shaders and water shader

---
+++ SUMmerHarvest/Assets/Scripts/Game/GameObjects/Car.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public int DestPoint = 0;
+    public Transform navPoints;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+        GotoNextPoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+	}
+
+    void GotoNextPoint()
+    {
+        if (navPoints.childCount <= 0) return;
+
+        agent.destination = navPoints.GetChild(DestPoint).position;
+
+        DestPoint = (DestPoint + 1) % navPoints.childCount;
+    }
+}
